cfeeb3c4f3c666500eb352b671a6adf966108a63
Lucas Zerino <101360532+LucasZerino@users.noreply.github.com>
2022-04-28 14:55:49 -0300

    Add files via upload

--- assets/js/chart.morris.js
+++ assets/js/chart.morris.js
@@ -1,4 +1,6 @@
-$(document).ready(function(){lineChart();donutChart();pieChart();$(window).resize(function(){window.lineChart.redraw();window.donutChart.redraw();window.pieChart.redraw();});});function lineChart(){window.lineChart=Morris.Line({element:'line-chart',data:[{y:'2006',a:100,b:90},{y:'2007',a:75,b:65},{y:'2008',a:50,b:40},{y:'2009',a:75,b:65},{y:'2010',a:50,b:40},{y:'2011',a:75,b:65},{y:'2012',a:100,b:90}],xkey:'y',ykeys:['a','b'],labels:['Series A','Series B'],lineColors:['#009688','#cdc6c6'],lineWidth:'3px',resize:true,redraw:true});}
+$(document).ready(function(){lineChart();donutChart();pieChart();$(window).resize(function(){window.lineChart.redraw();window.donutChart.redraw();window.pieChart.redraw();});});function lineChart(){
+window.lineChart=Morris.Line({element:'line-chart',
+data:[{y:'2020',a:50,b:40},{y:'2021',a:55,b:55},{y:'2022',a:100,b:80}],xkey:'y',ykeys:['a','b'],labels:['Series A','Series B'],lineColors:['#61C8E7','#cdc6c6'],lineWidth:'3px',resize:true,redraw:true});}
 function donutChart()
 {window.donutChart=Morris.Donut({element:'donut-chart',
 data:[{label:"Quarto básico",value:50},
@@ -6,8 +8,8 @@ data:[{label:"Quarto básico",value:50},
 {label:"Quarto com Varanda",value:5},
 {label:"Quarto Duplo",value:10},
 {label:"Quarto de Luxo",value:10},],
-backgroundColor:'#f2f5fa',labelColor:'#009688',
-colors:['#0BA462','#39B580','#67C69D','#95D7BB'],
+backgroundColor:'#f2f5fa',labelColor:'#5B80F5',
+colors:['#5BF5C4','#61C8E7','#5EAFFF','#5B80F5','#5EFFFA'],
 resize:true,redraw:true});}
 function pieChart(){var paper=Raphael("pie-chart");
 paper.piechart(100,100,90,[18.373,18.686,2.867,23.991,9.592,0.213],
